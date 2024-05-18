009633677cfedd209b0c42a4f256c019354c3513
Matthias Bauer <matthias.bauer72@gmail.com>
2024-05-18 23:59:16 +0200

    messing with grass

--- Maze/Grass/GrassMultiMesh.cs
+++ Maze/Grass/GrassMultiMesh.cs
@@ -6,12 +6,54 @@ using System.Linq;
 public partial class GrassMultiMesh : MultiMeshInstance3D
 {
 	private GDScript _grassFactory;
+
+	[Export] public Mesh GrassMesh { get; set; }
 	
 	[Export] public Vector2 BladeWidth { get; set; } = new Vector2(0.01f, 0.02f);
 	[Export] public Vector2 BladeHeight { get; set; } = new Vector2(0.04f, 0.08f);
 	[Export] public Vector2 SwayYaw { get; set; } = new Vector2(0.0f, 10.0f);
 	[Export] public Vector2 SwayPitch { get; set; } = new Vector2(0.0f, 10.0f);
 	[Export] public float Density { get; set; } = 1.0f;
+	
+	public override void _Ready()
+	{
+		_grassFactory = ResourceLoader.Load<GDScript>("res://Maze/Grass/grass_factory.gd");
+		var mesh = new PlaneMesh();
+		mesh.Size = new Vector2(10, 10);
+		mesh.SubdivideWidth = 100;
+		mesh.SubdivideDepth = 100;
+		
+		var arraymesh = new ArrayMesh();
+		arraymesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, mesh.GetMeshArrays());
+		
+		PopulateMesh(arraymesh);
+	}
+	
+
+
+	public void PopulateMesh(Mesh mesh)
+	{
+		Multimesh = new MultiMesh();
+		Multimesh.Mesh = GrassMesh;
+		// call return empty variant for some reason...
+		var spawnPoints = _grassFactory.Call("generate", mesh, Density, BladeWidth, BladeHeight, SwayPitch, SwayYaw)
+			.AsGodotArray();
+		if(spawnPoints.Count == 0)
+			return;
+
+		Multimesh.TransformFormat = MultiMesh.TransformFormatEnum.Transform3D;
+		Multimesh.UseCustomData = true;
+		Multimesh.UseColors = true;
+		Multimesh.InstanceCount = spawnPoints.Count;
+
+		foreach(var index in Enumerable.Range(0, spawnPoints.Count))
+		{
+			var spawn = spawnPoints[index].AsGodotArray();
+			var basis = new Basis(Vector3.Up, Mathf.DegToRad(GD.RandRange(0, 359)));
+			Multimesh.SetInstanceTransform(index, spawn[0].AsTransform3D());
+			Multimesh.SetInstanceCustomData(index, spawn[1].AsColor());
+		}
+	}
 
 
 	public void PopulateMeshes(Godot.Collections.Array meshes)
@@ -47,11 +89,7 @@ public partial class GrassMultiMesh : MultiMeshInstance3D
 	
 	
 	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_grassFactory = ResourceLoader.Load<GDScript>("res://Maze/Grass/GrassFactory.gd");
-	}
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
